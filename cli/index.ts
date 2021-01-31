bc7ae3e852e5ae115080f3b97886910bfedb7470
Igor Golopolosov <igolopolosov@gmail.com>
2021-01-31 23:28:09 +0100

    fix(fs): provide working directory

--- cli/index.ts
+++ cli/index.ts
@@ -2,14 +2,14 @@ import { run } from '../runner'
 import * as fs from 'fs'
 import * as path from 'path'
 
-const cli = async () => {
-  const configPath = path.resolve('stats.config.js')
+export const cli = async (workingDirectory: string) => {
+  const configPath = path.resolve(workingDirectory, 'stats.config.js')
   const config = require(configPath)
   const report = await run(config)
 
   const templateHtml = path.resolve(__dirname, '../report-viewer/index.html')
-  const outputHtml = path.resolve('report.html')
-  const otputJson = path.resolve('report.json')
+  const outputHtml = path.resolve(workingDirectory, 'report.html')
+  const otputJson = path.resolve(workingDirectory, 'report.json')
   const htmlReport = fs
     .readFileSync(templateHtml, 'utf-8')
     .replace('%REPORT_JSON%', JSON.stringify(report))
@@ -18,5 +18,3 @@ const cli = async () => {
   fs.writeFileSync(outputHtml, htmlReport)
   console.log('generated report file at', outputHtml)
 }
-
-cli()
