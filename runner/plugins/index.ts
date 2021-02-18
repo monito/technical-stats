97ebe1c0e5471622b956b6e02c03936b9d930569
Igor Golopolosov <igolopolosov@gmail.com>
2021-02-18 23:37:39 +0100

    feat(circleci): add circleci plugin fetching version

--- runner/plugins/index.ts
+++ runner/plugins/index.ts
@@ -1,3 +1,4 @@
+export { circleci } from './plugin-circleci';
 export { docker } from './plugin-docker';
 export { github } from './plugin-github';
 export { typescript } from './plugin-typescript';
---
+++ runner/plugins/plugin-circleci.ts
@@ -0,0 +1,33 @@
+import { gql } from 'graphql-request'
+import { client } from '../core/api'
+import { PluginInput } from '../types'
+
+const QUERY = gql`
+  query CircleCi($owner: String!, $name: String!, $configPath: String!) {
+    repository(owner: $owner, name: $name) {
+      circleciConfig: object(expression: $configPath) {
+        ... on Blob {
+          text
+        }
+      }
+    }
+  }
+`
+
+export async function circleci (project: PluginInput) {
+  const { owner, name, defaultBranchName } = project
+  const { repository } = await client.request(QUERY, {
+    owner,
+    name,
+    configPath: `${defaultBranchName}:.circleci/config.yml`
+  })
+  if (!repository.circleciConfig) {
+    return {}
+  }
+  const circleciConfig = repository.circleciConfig.text
+  const firstLine = circleciConfig.split('\n')[0]
+  const configVersion = firstLine.split(':').pop().trim()
+  return {
+    configVersion
+  }
+}
