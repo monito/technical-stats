db36f22616d56ee7872ff356b92dea961619ea4d
Igor Golopolosov <igolopolosov@gmail.com>
2021-05-21 15:28:01 +0200

    test(plugins): add tests for circleci and docker plugins

---
+++ runner/plugins/__tests__/plugin-circleci.spec.ts
@@ -0,0 +1,42 @@
+import { circleci } from '../plugin-circleci'
+import { client } from '../../core/api'
+
+jest.mock('../../core/api', () => ({
+  client: {
+    request: jest.fn().mockImplementation(() => Promise.resolve({
+      repository: {
+        circleciConfig: {
+          text: 'version: 2.1'
+        }
+      }
+    }))
+  }
+}))
+
+describe('circleci', () => {
+  const runPlugin = async () => await circleci({
+    owner: 'monito',
+    repo: 'monito/technical-stats',
+    name: 'technical-stats',
+    defaultBranchName: 'main',
+  })
+
+  it('return empty object if no circle ci config was found', async () => {
+    (client.request as unknown as jest.Mock).mockImplementationOnce(() => Promise.resolve({
+      repository: {
+        circleciConfig: undefined
+      }
+    }))
+    const output = await runPlugin()
+
+    expect(output).toEqual({})
+  })
+
+  it('fetches circle config and parses it', async () => {
+    const output = await runPlugin()
+
+    expect(output).toEqual({
+      version: 2.1
+    })
+  })
+})
---
+++ runner/plugins/__tests__/plugin-docker.spec.ts
@@ -0,0 +1,43 @@
+import { docker } from '../plugin-docker'
+import { client } from '../../core/api'
+
+jest.mock('../../core/api', () => ({
+  client: {
+    request: jest.fn().mockImplementation(() => Promise.resolve({
+      repository: {
+        dockerfile: {
+          text: 'FROM node:12-slim'
+        }
+      }
+    }))
+  }
+}))
+
+describe('docker', () => {
+  const runPlugin = async () => await docker({
+    owner: 'monito',
+    repo: 'monito/technical-stats',
+    name: 'technical-stats',
+    defaultBranchName: 'main',
+  })
+
+  it('return empty object if no Dockerfile config was found', async () => {
+    (client.request as unknown as jest.Mock).mockImplementationOnce(() => Promise.resolve({
+      repository: {
+        dockerfile: undefined
+      }
+    }))
+    const output = await runPlugin()
+
+    expect(output).toEqual({})
+  })
+
+  it('fetches Dockerfile and returns image and image version', async () => {
+    const output = await runPlugin()
+
+    expect(output).toEqual({
+      image: 'node:12-slim',
+      imageVersion: '12-slim'
+    })
+  })
+})
